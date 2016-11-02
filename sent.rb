84f87cf0380da9b73e45b7604521046ef0db475a
Patrick O'Brien <pobrien@pobrien.local>
2016-11-01 20:43:28 -0700

    removing powershell; fixing sent

--- sent.rb
+++ sent.rb
@@ -4,8 +4,8 @@ class Sent < Formula
   url "http://dl.suckless.org/tools/sent-0.2.tar.gz"
   sha256 "53b961f9d92a277a6408df7025b4a6deae6b655a797383c93442290e45391076"
 
-  depends_on :x11
-  depends_on :freetype
+  depends_on "x11"
+  depends_on "freetype"
 
   def install
     patch do
